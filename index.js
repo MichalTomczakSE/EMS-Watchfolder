194da6522ba7c3b3626be827df7b000e78adfaec
Michał Tomczak <MichalTomczakSe@gmail.com>
2022-12-09 17:25:59 +0100

    Simple process.argv destruct in index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const watchFolder = process.argv[2];
-const transcodingFolder = process.argv[3];
-const destinationFolder = process.argv[4];
-
+const [,watchFolder,transcodingFolder,destinationFolder] = process.argv;
 const {ingestFolder} = require("./watchfolders/source");
-
 
 ingestFolder(watchFolder,transcodingFolder);
 
