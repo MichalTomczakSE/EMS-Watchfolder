9d7276833ed47ea54f49c8bbe4beac1bd536c6e7
Michał Tomczak <MichalTomczakSe@gmail.com>
2022-12-09 16:03:57 +0100

    Add ffmpeg and scaling image function

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const {watch} = require('chokidar');
-const {readFile, writeFile, unlink, rename} = require('fs').promises;
+const {readFile, writeFile, unlink} = require('fs').promises;
 const {basename, extname} = require('path');
+const ffmpeg = require('fluent-ffmpeg');
+
 const watchFolder = process.argv[2];
 const transcodingFolder = process.argv[3];
+
 const {colors} = require('./utils/colors');
 const {green, yellow, red, white} = colors;
 const {acceptableFileExtensions: extensions} = require('./data/extensions');
@@ -31,8 +34,17 @@ watch(`${watchFolder}`, {
             data.id = data.files.length;
 
             await writeFile('./data/data.json', JSON.stringify(data));
-            await rename(`${watchFolder}/${fileName}`,`${transcodingFolder}/${fileName.slice(0,10)}${extname(path)}/`)
-            console.log('File has been successfully saved in database, and moved to transcoder!')
+
+            console.log('File has been successfully saved in database, and moved to transcoder!');
+
+            await ffmpeg(`${watchFolder}/${fileName}`)
+                .size('1920x1080')
+                .on('end', async() => {
+                    await unlink(`${watchFolder}/${fileName}`)
+                    console.log(`${green} file scaled and copied successfully! Generating video starting soon...`)
+                })
+                .output(`${transcodingFolder}/${fileName.slice(0,10)}`)
+                .run();
 
         } catch (error) {
             if (error) {
