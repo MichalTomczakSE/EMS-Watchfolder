5420bfba31f60e76aa84f150365615f156f69b92
Michał Tomczak <MichalTomczakSe@gmail.com>
2022-12-10 17:52:25 +0100

    Unlinking file after successful transcoding

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,18 +1,21 @@
-const {white, red} = require("./colors");
+const {unlink} = require('fs').promises;
+const {colors} = require("./colors");
+const {white, red} = colors;
 
-const ffmpegOutputHelper = ffmpeg => {
+const ffmpegOutputHelper = (ffmpeg,filePath) => {
 
-ffmpeg.stdout.on('data', (data) => {
-    console.log(`${white}stdout: ${data}`);
-});
+    ffmpeg.stdout.on('data', (data) => {
+        console.log(`${white}stdout: ${data}`);
+    });
 
-ffmpeg.stderr.on('data', (data) => {
-    console.error(`${red}stderr: ${data}`);
-});
+    ffmpeg.stderr.on('data', (data) => {
+        console.error(`${red}stderr: ${data}`);
+    });
 
-ffmpeg.on('close', (code) => {
-    console.log(`${white}child process exited with code ${code}`);
-});
+    ffmpeg.on('close', async (code) => {
+        console.log(`${white}child process exited with code ${code}`);
+        await unlink(`${filePath}`)
+    });
 };
 
 module.exports = {
