442a0161861df273fe6850ec079ca146cb2c213d
Michał Tomczak <MichalTomczakSe@gmail.com>
2022-12-11 12:58:45 +0100

    Match parameters to constant values

--- utils/createVideo.js
+++ utils/createVideo.js
@@ -1,16 +1,16 @@
 const { spawn } = require('child_process');
 const {ffmpegOutputHelper} = require("./errorHandler");
-const createVideo = (background, fileName) => {
+const createVideo = (sequenceFolder,background, destination, fileName) => {
 
     const ffmpeg = spawn('ffmpeg', [
 
-        '-i', `C:\\PRACA\\EMS_WATCHFOLDER\\data\\sequence\\ramka_%05d.png`,
-        '-i', `${background}`, '-s', '1920x1080',
+        '-i', sequenceFolder,
+        '-i', background, '-s', '1920x1080',
         '-filter_complex', '[1:v][0:v]overlay=0:0[out]',
         '-map', '[out]',
         '-c:v', 'libx264',
         '-b:v', '5M',
-        `C:\\destination\\${fileName.slice(0,10)}.mp4`
+        `${destination}/${fileName.slice(0,10)}.mp4`
     ]);
 
     ffmpegOutputHelper(ffmpeg,background);
