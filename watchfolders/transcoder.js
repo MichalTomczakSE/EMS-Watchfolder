1f7923126de890a53ccf98926e57b00bc5c617fb
Michał Tomczak <MichalTomczakSe@gmail.com>
2022-12-11 12:56:11 +0100

    Set new parameters for videoTranscoder function

--- watchfolders/transcoder.js
+++ watchfolders/transcoder.js
@@ -1,23 +1,21 @@
 const {watch} = require("chokidar");
-const {unlink} = require('fs').promises;
 const {basename, extname} = require('path');
 const {colors} = require('../utils/colors');
 const {createVideo} = require("../utils/createVideo");
 const {green, yellow, red, white} = colors;
 
 
-const videoTranscoder = (transcodingFolder, destinationFolder) => {
+const videoTranscoder = (sequenceFolder, transcodingFolder, destinationFolder) => {
 
     watch(`${transcodingFolder}`, {
         usePolling: true,
         awaitWriteFinish: true,
         ignoreInitial: true,
     }).on('add', async (path) => {
-        console.log('basename in transcoder.js: ', transcodingFolder, basename(path))
-        await createVideo(`${transcodingFolder}/${basename(path)}`,basename(path, extname(path)))
+        await createVideo(sequenceFolder, `${transcodingFolder}/${basename(path)}`, destinationFolder, basename(path, extname(path)))
     })
         .on('error', error => console.log(`${red}Error:`, error))
-        .on('unlink', (path) => console.log(`${yellow}File ${basename(path)} has been deleted.`))
+        .on('unlink', (path) => console.log(`${yellow}File ${basename(path)} has been deleted.${white}`))
         .on('ready', () => console.log(`${green}Initial scan complete. Ready for changes`));
 }
 
