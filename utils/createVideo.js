f23cee7576f27190969faf9290ce76a0b9f87279
Michał Tomczak <MichalTomczakSe@gmail.com>
2022-12-14 20:40:21 +0100

    Refactor createVideo function to match processes.json data

--- utils/createVideo.js
+++ utils/createVideo.js
@@ -1,19 +1,32 @@
-const { spawn } = require('child_process');
+const {spawn} = require('child_process');
 const {ffmpegOutputHelper} = require("./errorHandler");
-const createVideo = (sequenceFolder,background, destination, fileName) => {
-
+const createVideo = (sequenceFolder, background, destination, fileName) => {
+    const {
+        input,
+        scale,
+        resolution,
+        filter,
+        filterOptions,
+        mapper,
+        mapOutput,
+        setVideoCodec,
+        videoCodec,
+        setVideoBitrate,
+        videoBitrate
+    } = require('../data/processes.json')
     const ffmpeg = spawn('ffmpeg', [
 
-        '-i', sequenceFolder,
-        '-i', background, '-s', '1920x1080',
-        '-filter_complex', '[1:v][0:v]overlay=0:0[out]',
-        '-map', '[out]',
-        '-c:v', 'libx264',
-        '-b:v', '5M',
-        `${destination}/${fileName.slice(0,10)}.mp4`
+        input, sequenceFolder,
+        input, background,
+        scale, resolution,
+        filter, filterOptions,
+        mapper, mapOutput,
+        setVideoCodec, videoCodec,
+        setVideoBitrate, videoBitrate,
+            `${destination}/${fileName.slice(0, 10)}.mp4`
     ]);
 
-    ffmpegOutputHelper(ffmpeg,background);
+    ffmpegOutputHelper(ffmpeg, background);
 }
 
 module.exports = {
