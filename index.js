044bc213be027b4ef491f6a452df3f1084f1e135
Michał Tomczak <MichalTomczakSe@gmail.com>
2022-12-14 21:02:19 +0100

    Get only used processes from json data

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const pathTo = require('./data/processes.json')
-
+const {watchFolder, transcodingFolder, destinationFolder, sequenceFolder} = require('./data/processes.json')
 const {ingestFolder} = require("./watchfolders/source");
 const {videoTranscoder} = require("./watchfolders/transcoder");
-ingestFolder(pathTo.watchFolder,pathTo.transcodingFolder);
-videoTranscoder(pathTo.sequenceFolder,pathTo.transcodingFolder,pathTo.destinationFolder);
+ingestFolder(watchFolder,transcodingFolder);
+videoTranscoder(sequenceFolder,transcodingFolder,destinationFolder);
