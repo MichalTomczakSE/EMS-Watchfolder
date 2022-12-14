d3385bca513faa735ad9304a9ec1da1f0791f666
Michał Tomczak <MichalTomczakSe@gmail.com>
2022-12-14 21:03:22 +0100

    Add framerate to ffmpeg function

--- utils/createVideo.js
+++ utils/createVideo.js
@@ -3,6 +3,8 @@ const {ffmpegOutputHelper} = require("./errorHandler");
 const createVideo = (sequenceFolder, background, destination, fileName) => {
     const {
         input,
+        framerate,
+        framerateValue,
         scale,
         resolution,
         filter,
@@ -15,7 +17,7 @@ const createVideo = (sequenceFolder, background, destination, fileName) => {
         videoBitrate
     } = require('../data/processes.json')
     const ffmpeg = spawn('ffmpeg', [
-
+        framerate, framerateValue,
         input, sequenceFolder,
         input, background,
         scale, resolution,
@@ -23,7 +25,7 @@ const createVideo = (sequenceFolder, background, destination, fileName) => {
         mapper, mapOutput,
         setVideoCodec, videoCodec,
         setVideoBitrate, videoBitrate,
-            `${destination}/${fileName.slice(0, 10)}.mp4`
+        `${destination}/${fileName.slice(0, 10)}.mp4`
     ]);
 
     ffmpegOutputHelper(ffmpeg, background);
